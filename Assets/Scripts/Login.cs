bfd9edf2d1f37df67be6436dbeaff22b4ac430c6
dby1750 <1750666868@qq.com>
2021-08-09 13:13:12 +0800

    拷贝进去了

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Google.Protobuf;
+using XProgect;
+
+public class Login : MonoBehaviour
+{
+    public bool LogIn(string id ,string password)
+    {
+        if (File.Exists(Application.dataPath + "/" + name))
+        {
+            byte[] read = File.ReadAllBytes(Application.dataPath + "/" + name);
+            Gamedata message = new Gamedata();
+            message.MergeFrom(read);
+            if (password==message.Password)
+            {
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
