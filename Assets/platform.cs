5ad9ed53f5c07b4a891d503e833e8e08e50ebd0f
ZJH <1142051835@qq.com>
2021-08-21 17:09:21 +0800

    UI优化基本完毕

---
+++ Assets/platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ABFW;
+
+public class platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string s = PathTools.GetABOutPath();
+        Debug.Log(Application.platform);
+        Debug.Log(s);
+        Debug.Log(Application.dataPath + "/" + "AB_Resources");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
