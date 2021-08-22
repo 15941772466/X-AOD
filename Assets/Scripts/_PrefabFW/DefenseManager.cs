f569a6bcc9ac909624289c1d4e2836cf998adff7
DZL <15034966931@163.com>
2021-08-22 10:05:14 +0800

    dzl:解决打包bug

--- Assets/Scripts/_PrefabFW/DefenseManager.cs
+++ Assets/Scripts/_PrefabFW/DefenseManager.cs
@@ -135,8 +135,8 @@ namespace PFW
             foreach (var item in DTPaths)
             {
                 string refRoad = item.Value;
-                var goPrefab = Resources.Load<GameObject>(refRoad);
-                //var goPrefab = AssetDatabase.LoadAssetAtPath("Assets/" + "AB_Resources/" + refRoad, typeof(GameObject)) as GameObject;
+                //var goPrefab = Resources.Load<GameObject>(refRoad);
+                var goPrefab = AssetDatabase.LoadAssetAtPath("Assets/" + "AB_Resources/" + refRoad, typeof(GameObject)) as GameObject;
                 string pName = goPrefab.name;
                 //委托调用
                 _DicALLDTForms.Add(pName, goPrefab);
